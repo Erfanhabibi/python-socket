d74401677128e10a8435b42b1ce27d67ba0132dd
Erfanhabibi <erfan.habibi.ehsani@gmail.com>
2024-03-27 12:38:24 +0330

    Update function to handle edge cases

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+
+# Create the socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the localhost:10001
+server_address = ('localhost', 10001)
+print('connecting to %s:%s' % server_address)
+sock.connect(server_address)
+
+message = ''
+
+try:
+    while message != 'done':
+        # read the request
+        message = input('\nWhat is your math question? [sample: 4*7+6] ')
+        # send the query
+        sock.sendall(message.encode('UTF-8'))
+
+        # read the answer from the server
+        data = sock.recv(100) #it should not be more than 100 chars
+        print('The answer of %s is %s.' % (message, data.decode('UTF-8')))
+
+finally:
+    print('closing socket', file=sys.stderr)
+    sock.close()
---
+++ server.py
@@ -0,0 +1,40 @@
+import socket
+import sys
+
+# Create the socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the localhost:10001
+server_address = ('localhost', 10001)
+sock.bind(server_address)
+print('starting up on %s port %s' % server_address)
+
+# Listen
+sock.listen(1)
+
+while True:
+    # Wait for an incomming connection
+    print('waiting...', file=sys.stderr)
+    connection, client_addr = sock.accept()
+
+    try:
+        print('A request came! IP is ', client_addr)
+
+        # Read data
+        while True:
+            data = connection.recv(150)  # or any other number
+            print('request is:', data.decode('UTF-8'))
+            if data:
+                try:
+                    answer = str(eval(data))
+                except:
+                    answer = 'error!'
+                connection.sendall(answer.encode('UTF-8'))
+                print('Hooraay.... Answered this one.')
+            else:
+                print('No data recived from', client_addr)
+                break
+
+    finally:
+        # Clean up the connection
+        connection.close()
